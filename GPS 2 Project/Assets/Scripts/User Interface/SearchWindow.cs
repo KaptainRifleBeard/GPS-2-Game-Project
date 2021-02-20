7ec5b6826fe1d07275b9bac6c628e86783fcd256
SelinaLim <escales1018@gmail.com>
2021-02-21 05:32:43 +0800

    Search Window Pocket done

--- GPS 2 Project/Assets/Scripts/User Interface/SearchWindow.cs
+++ GPS 2 Project/Assets/Scripts/User Interface/SearchWindow.cs
@@ -6,40 +6,63 @@ using UnityEngine.UI;
 
 public class SearchWindow : MonoBehaviour
 {
-    public Text name;
+    public Text n;
     public Text value;
     public Text space;
 
-
     public ProrgessBar bar;
-    public Text spaceCount;
-    private int spaceUsedPocket;
-    private int maxPocket = 5;
+    public static int maxPocket = 5;
+    public Text maxSpace;
+
+    public static int total;
+
+    int i;
+    public int num;
 
 
     [Header("List of the items")]
     [Space(50)]
     public List<WindowManager> windowManage = new List<WindowManager>();
-    
+   
 
-
-    private void Update()
+    private void Start()
     {
+
+    }
+
+    public void Update()
+    {
+
         if (bar.showWindow == true)
         {
+            maxSpace.text = " / " +  maxPocket.ToString();
 
             //! check level int to determine rand value, each level have diff amount
             //! SET setSctive to limit panel amount show
             int rand = Random.Range(1, windowManage.Count);
 
-            for(int i = 0; i < rand; i++)
+            for (i = 1; i < rand; i++)
             {
-                name.text = windowManage[i].itemName;
+                n.text = windowManage[i].itemName;
                 value.text = windowManage[i].itemValue;
+
+                num = windowManage[i].spaceNeed;
                 space.text = windowManage[i].spaceNeed.ToString();
+
             }
-            spaceCount.GetComponent<Text>().text = spaceUsedPocket.ToString() + " / " + maxPocket.ToString();
+
+           
         }
     }
 
+
+    public void TakeButton()
+    {
+        Debug.Log(num);
+
+        if (total < maxPocket)
+        {
+            total += num;
+        }
+    }
 }
