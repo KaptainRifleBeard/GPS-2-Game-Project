4778749260a9bdc736b3a5aafde9d24d36424118
SelinaLim <escales1018@gmail.com>
2021-02-21 22:41:31 +0800

    Searchable object window

--- GPS 2 Project/Assets/Scripts/User Interface/ButtonValue.cs
+++ GPS 2 Project/Assets/Scripts/User Interface/ButtonValue.cs
@@ -86,45 +86,45 @@ public class ButtonValue : MonoBehaviour
            n.text == "Silver pocket watch" || n.text == "Gold and Jade Necklace")
         {
             SearchWindow.totalSafeCount -= 1;
-            pocketName.text = " ";
-            pocketValue.text = " ";
-            pocketSpace.text = " ";
+            pocketName.text = null;
+            pocketValue.text = null;
+            pocketSpace.text = null;
         }
 
         if (n.text == "Designer sunglasses" || n.text == "Cashmere designer pink shawl" || n.text == "Pendant locket necklace" ||
            n.text == "Gold and diamond  bracelet" || n.text == "Mobile Phone" || n.text == "Antique intricately engraved large pocket watch")
         {
             SearchWindow.totalSafeCount -= 2;
-            pocketName.text = " ";
-            pocketValue.text = " ";
-            pocketSpace.text = " ";
+            pocketName.text = null;
+            pocketValue.text = null;
+            pocketSpace.text = null;
         }
 
         if (n.text == "Gold ceramic cigar tray" || n.text == "Designer female tote bag" || n.text == "Small wooden antique clock" ||
            n.text == "Double crystal drinking glass" || n.text == "Antique Malaysian wooden smoking pipe")
         {
             SearchWindow.totalSafeCount -= 3;
-            pocketName.text = " ";
-            pocketValue.text = " ";
-            pocketSpace.text = " ";
+            pocketName.text = null;
+            pocketValue.text = null;
+            pocketSpace.text = null;
         }
 
         if (n.text == "Antique golden candle holder" || n.text == "Small abstract glass male and female sculpture" || n.text == "Female hand painted porcelain figurine" ||
            n.text == "Golden diamond encrusted wedding tiara")
         {
             SearchWindow.totalSafeCount -= 4;
-            pocketName.text = " ";
-            pocketValue.text = " ";
-            pocketSpace.text = " ";
+            pocketName.text = null;
+            pocketValue.text = null;
+            pocketSpace.text = null;
         }
 
         if (n.text == "High end Laptop" || n.text == "Antique decorative plate" || n.text == "Antique Silver gold encrusted teapot" ||
            n.text == "Golden Dragon Statue")
         {
             SearchWindow.totalSafeCount -= 5;
-            pocketName.text = " ";
-            pocketValue.text = " ";
-            pocketSpace.text = " ";
+            pocketName.text = null;
+            pocketValue.text = null;
+            pocketSpace.text = null;
         }
     }
 
