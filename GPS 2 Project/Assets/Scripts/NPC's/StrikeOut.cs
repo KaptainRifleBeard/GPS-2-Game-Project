81e29df6c60c3b77df6baea1959786034af03769
Thea Hoo <0126943@kdu-online.com>
2021-02-22 16:56:05 +0800

    Updated Npc behavior and StrikeOut meter

---
+++ GPS 2 Project/Assets/Scripts/NPC's/StrikeOut.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StrikeOut : MonoBehaviour
+{
+    public int currSus;
+    public int maxSus = 3;
+    public int currStrike;
+    public int maxStrike = 3;
+    static public bool sus = false;
+    public Text susText;
+    public Text strikeText;
+    public GameObject loseScreenUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currSus = 0;
+        currStrike = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(NpcMovement.sus)
+        {
+            currSus += 1;
+            sus = false;
+        }
+
+        if(currSus >= maxSus)
+        {
+            currStrike += 1;
+            currSus -= 1;
+        }
+
+        if(currStrike >= maxStrike)
+        {
+            //SceneManager.LoadScene("LoseScreen");
+            loseScreenUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        susText.GetComponent<Text>().text = "Suspicion level: " + currSus.ToString() + "/" + maxSus.ToString();
+        strikeText.GetComponent<Text>().text = "Strike Out: " + currStrike.ToString() + "/" + maxStrike.ToString();
+
+    }
+}
