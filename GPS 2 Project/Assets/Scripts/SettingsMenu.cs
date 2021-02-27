1af04b3562347199cf69d6454f40b3e2bda7762b
Nizmarck <0129974@kdu-online.com>
2021-02-28 01:06:37 +0800

    added basic bgm and SFX enable disable button , settings script
    settings script needs and requires more work placed into it, needs mr zerens's guidance on how to make button groups.

---
+++ GPS 2 Project/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Button button;
+    public Slider slider;
+    public Text Text;
+    public CanvasGroup canvgrp1;
+    public CanvasGroup canvgrp2;
+
+    public void BGMbutton(Text text)
+    {
+        if (PlayerPrefs.GetInt("BGM")== 1)
+        {
+
+            PlayerPrefs.SetInt("BGM", -1);
+            
+        }
+        else if (PlayerPrefs.GetInt("BGM") == -1)
+        {
+
+            PlayerPrefs.SetInt("BGM", 1);
+
+        }
+    }
+
+    public void SFXbutton()
+    {
+        if (PlayerPrefs.GetInt("SFX") == 1)
+        {
+
+            PlayerPrefs.SetInt("SFX", -1);
+
+        }
+        else if (PlayerPrefs.GetInt("SFX") == -1)
+        {
+
+            PlayerPrefs.SetInt("SFX", 1);
+
+        }
+    }
+
+    public void SFXSlider(Slider slider)
+    {
+        if (slider.normalizedValue>0.00)
+        {
+
+            PlayerPrefs.SetFloat("SFXvolume", slider.normalizedValue);
+            PlayerPrefs.SetInt("SFX", 1);
+
+        }
+        else if (slider.normalizedValue == 0.00)
+        {
+
+            PlayerPrefs.SetFloat("SFXvolume", slider.normalizedValue);
+            PlayerPrefs.SetInt("SFX", -1);
+
+        }
+    }
+
+
+
+    public void BGMSlider(Slider slider)
+    {
+        if (slider.normalizedValue > 0.00)
+        {
+
+            PlayerPrefs.SetFloat("BGMvolume", slider.normalizedValue);
+            PlayerPrefs.SetInt("BGM", 1);
+
+        }
+        else if (slider.normalizedValue == 0.00)
+        {
+
+            PlayerPrefs.SetFloat("BGMvolume", slider.normalizedValue);
+            PlayerPrefs.SetInt("BGM", -1);
+
+        }
+    }
+
+    public void soundSetting()
+    {
+
+    }
+
+    public void returntosettings()
+    {
+
+    }
+
+}
