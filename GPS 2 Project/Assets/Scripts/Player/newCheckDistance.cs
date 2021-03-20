ae9a5adc8b864c6fd195f5c91bbee089c01e7d2d
SelinaLim <escales1018@gmail.com>
2021-03-20 18:57:43 +0800

    Fixed highlight

--- GPS 2 Project/Assets/Scripts/Player/newCheckDistance.cs
+++ GPS 2 Project/Assets/Scripts/Player/newCheckDistance.cs
@@ -7,52 +7,36 @@ public class newCheckDistance : MonoBehaviour
     public Transform player;
 
     //To highlight object
-    public Material highlightMat;
-    public Material defaultMat;
     public Renderer mat;
 
-    public Material[] hMat;
-
+    public Color highlightColor;
+    public List<Color> defaultColor;
 
 
     public GameObject searchButton;
     public GameObject cleanButton;
-
-    IEnumerator blink()
-    {
-        for (int n = 0; n < 100; n++)
-        {
-            mat.material.color = Color.green;
-            yield return new WaitForSeconds(3.0f);
-            mat.material.color = Color.white;
-            yield return new WaitForSeconds(3.0f);
-        }
-    }
-
 
     public void OnCollisionEnter(Collision collision)
     {
         
         if (collision.collider.tag == "Player")
         {
-            searchButton.SetActive(true);
             if (gameObject.tag == "CleaningTaskObject")
             {
-
                 Debug.Log("Task");
                 cleanButton.SetActive(true);
 
             }
-
-
-            //highlight
-            for (int i = 0; i < mat.materials.Length;)
+            else
             {
-                mat.material = highlightMat;
-                i++;
+                searchButton.SetActive(true);
+                for (int i = 0; i < mat.materials.Length; i++)
+                {
+                    mat.materials[i].color = highlightColor;
+                }
+               
+                
             }
-
-
 
         }
     }
@@ -62,29 +46,28 @@ public class newCheckDistance : MonoBehaviour
     {
         searchButton.SetActive(false);
         cleanButton.SetActive(false);
-
         for (int i = 0; i < mat.materials.Length; i++)
         {
-            mat.material = defaultMat;
+            mat.materials[i].color = defaultColor[i];
         }
-
-        //loop store default color
     }
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        defaultMat = GetComponent<Renderer>().material;
+        for (int i = 0; i < mat.materials.Length; i++)
+        {
+            defaultColor.Add(mat.materials[i].color);
+        }
 
     }
 
     void Update()
     {
-        if(gameObject.tag == "CleaningTaskObject")
-        {
-            mat.material.color = Color.green;
+        //if(gameObject.tag == "CleaningTaskObject")
+        //{
+        //    mat.material.color = Color.green;
 
-            //StartCoroutine(blink());
-        }
+        //}
     }
 }
