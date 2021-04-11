44f7bf34155a86edf5b647e7e8ae03a1872fafd7
Nizmarck <0129974@kdu-online.com>
2021-04-11 17:41:44 +0800

    Added LevelSelect Screen
    Upon Completion of level call script and assign Completion level from 1 to 3

---
+++ GPS 2 Project/Assets/Scripts/LevelSelect1.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelSelect1 : MonoBehaviour
+{
+    private bool unlocked;
+    private int levelCompleted = 0;
+
+   // public int LevelSelected;
+    public int ThisLevel;
+
+    public Sprite star;
+    public Sprite Locked;
+    public Sprite star1, star2, star3;
+
+
+    public void clicked(int nextLevel)
+    {
+        if (ThisLevel - levelCompleted >= -1)
+        {
+            SceneManager.LoadScene(nextLevel);
+        }   else
+        {
+            return;
+        }
+    }
+
+    public void clearLevel(int starLevel)
+    {
+        if (starLevel == 1)
+        {
+            this.gameObject.GetComponent<Image>().sprite = star1;
+
+        }
+        else if (starLevel ==2)
+        {
+            this.gameObject.GetComponent<Image>().sprite = star2;
+        }
+        else if (starLevel ==3)
+        {
+            this.gameObject.GetComponent<Image>().sprite = star3;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelCompleted - ThisLevel >= 1)
+        {
+            this.gameObject.GetComponent<Image>().sprite = star;
+        }else if (levelCompleted - ThisLevel >= 1)
+        {
+            this.gameObject.GetComponent<Image>().sprite = Locked;
+        }
+
+
+    }
+}
